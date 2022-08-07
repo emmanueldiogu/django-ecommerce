1fb2a030f57d88e03f88db1351e731e484fd9cd5
Emmanuel Diogu <emmanueldiogu@gmail.com>
2022-08-07 16:59:43 +0100

    part update 1

--- storefront2/settings.py
+++ storefront2/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from datetime import timedelta
 import os
 from pathlib import Path
+from celery.schedules import crontab
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -190,3 +191,13 @@ ADMINS = [
 
 
 CELERY_BROKER_URL = 'redis://localhost:6379/1'
+CELERY_BEAT_SCHEDULE = {
+    'notify_customers': {
+        'task': 'playground.tasks.notify_customers',
+        'schedule': 5,
+        'args': ['Hello World'],
+        # 'schedule': crontab(day_of_week=1, hour=7, minute=30) #every Monday by 7:30am
+        # 'schedule': crontab(minute='*/15) this would run every 15 seconds
+        # 'kwarg' : {}
+    }
+}
